75070042a2d1a816d921a61c8aa8cdcbafd80a9d
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-04-28 23:47:48 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    @user=User.find_by(:username => params[:username])##your code here
   if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect to "/account"
